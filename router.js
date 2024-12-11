6a0f87c6300af581dc9e087b807a53b3be7202dd
Gleb Kushedow <kushedow@gmail.com>
2024-12-12 01:24:06 +0300

    add Express-style params to router

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const Router = {
-  routes: { GET: {}, POST: {} },
+
+  routes: { GET: {}, POST: {} }, // type Route = 
 
   get(path, callable) {this.add("GET", path, callable);},
   post(path, callable) {this.add("POST", path, callable);},
@@ -9,24 +10,44 @@ const Router = {
     this.validateCallable(callable); 
     this.routes[method][path] = callable;
   },
-  open(method = "GET", path = "",  request = {}) {
+
+  matchRoute(requestPath, routes){
+
+    const requestSegments = requestPath.split("/")
+    for ([routePath, callable] of Object.entries(routes)){
+        const params = {}
+        let match = true
+        const routeSegments = routePath.split("/")
+        if (requestSegments.length !== routeSegments.length){continue}  
+        for (const [index, oneRouteSegment] of routeSegments.entries()) {
+           if (oneRouteSegment[0] == ":") { params[oneRouteSegment] = requestSegments[index]; continue; }
+           if (oneRouteSegment !== requestSegments[index]){ match = false }
+        }
+        if (match) {return {path: routePath, callable: callable, params: params}}
+    }
+  },
+
+  open(method = "GET", path = "",  request = {}, json=true) {
+
     try {
-       if (!this.routeExists(method, path)) {this.throw(`NOT FOUND ${method} ${path}`);}
-       result = this.routes[method][path](request);
+       route = this.matchRoute(path, this.routes[method])
+       if (!route) {this.throw(`NOT FOUND ${method} ${path}`);}
+       request.params = route.params
+       result = route.callable(request);
+
     } catch(error){
        return this.toJSON({"error": error.toString() }) 
     }
-    return this.toJSON(result)
+    return json ? this.toJSON(result) : result
   },
   validateMethod(method) { if (!["POST", "GET"].includes(method)) {this.throw(`Unsupported Method ${method}`);}},
   validatePath(path){ if (!path[0]=="/") {this.throw(`Route ${path} should start with /`); }},
   validateCallable(callable) { if (!(callable instanceof Function)) { this.throw('Cannot add route handler, a Callable (function) is expected');}},
 
   toJSON(data){return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(ContentService.MimeType.JSON);},
-  routeExists(method, path) {return this.routes[method].hasOwnProperty(path);},
+  
   throw(message) { Logger.log(`❌ ${message}`); throw new Error(message);}
 };
 
 function doGet(req){ return Router.open("GET", "/"+ (req.pathInfo ? req.pathInfo : "") , req) }
 function doPost(req){ return Router.open("POST", "/"+ (req.pathInfo ? req.pathInfo : ""), req) }
-
